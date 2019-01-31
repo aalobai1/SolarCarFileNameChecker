d09c089e6346316e924dcc2a54aa6db0313b8de0
Ali Alobaidi <alialobaidi@alis-macbook-pro-6.local>
2019-01-30 19:03:11 -0500

    Initial Commit

---
+++ SolarCarFileNameChekcer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SolarCarFileNameChekcer
+//
+//  Created by Ali Alobaidi on 1/30/19.
+//  Copyright © 2019 Ali Alobaidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
